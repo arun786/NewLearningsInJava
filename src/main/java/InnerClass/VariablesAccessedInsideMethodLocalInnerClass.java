fef9b42a79194ac3189d0b3776c627d83911d8c5
arun789 <adwiti1975@gmail.com>
2018-01-13 12:46:01 -0700

    Method local inner classes

--- src/main/java/InnerClass/VariablesAccessedInsideMethodLocalInnerClass.java
+++ src/main/java/InnerClass/VariablesAccessedInsideMethodLocalInnerClass.java
@@ -2,7 +2,7 @@ package InnerClass;
 
 public class VariablesAccessedInsideMethodLocalInnerClass {
     private int instanceVariable = 10;
-    private static int staticVarible = 12;
+    private static int staticVariable = 12;
     private static final int staticFinalVariable = 14;
 
     /**
@@ -10,18 +10,22 @@ public class VariablesAccessedInsideMethodLocalInnerClass {
      */
     private void m1() {
 
-        int localVariable = 16;
+        /**
+         * local variable cannot be accessed from inner class if
+         * they are not final or effectively final
+         */
+        int localVariable = 12;
         final int finalLocalVariable = 18;
 
         class inner {
             private void m2() {
+                System.out.println("We cannot access local variable if it is not initialized.." + localVariable);
                 System.out.println("instance variable can be accessed " + instanceVariable);
-                System.out.println("static variable can be accessed " + staticVarible);
+                System.out.println("static variable can be accessed " + staticVariable);
                 System.out.println("static final variable can be accessed " + staticFinalVariable);
                 System.out.println("final local variable can be accessed " + finalLocalVariable);
             }
         }
-
         inner i = new inner();
         i.m2();
     }
--- src/main/java/InnerClass/VariablesAccessedInsideStaticMethodLocalInnerClass.java
+++ src/main/java/InnerClass/VariablesAccessedInsideStaticMethodLocalInnerClass.java
@@ -1,4 +1,37 @@
 package InnerClass;
 
+/**
+ * Method Local Inner class when declared inside a static method
+ */
 public class VariablesAccessedInsideStaticMethodLocalInnerClass {
+
+    private int instanceVariable = 10; //Cannot be accessed via a static method
+    private static int staticVariable = 12;
+    private static final int staticFinalVariable = 14;
+
+    /**
+     * Inner class defined inside a static method
+     */
+    public static void m1() {
+
+        int localVariable = 10;
+        final int localFinalVariable = 12;
+        class inner {
+            public void m2() {
+                System.out.println("We cannot access instance variable " + localVariable);
+                //System.out.println("Instance variable cannot be accessed "+instanceVariable);
+                System.out.println("static final variable can be accessed " + staticFinalVariable);
+                System.out.println("static variable can be accessed " + staticVariable);
+                System.out.println("local final variable can be accessed " + localFinalVariable);
+            }
+        }
+
+        inner i = new inner();
+        i.m2();
+    }
+
+    public static void main(String[] args) {
+        VariablesAccessedInsideStaticMethodLocalInnerClass.m1();
+    }
+
 }
