30329f69458d13a8cb7651fa2d3584df2447a09e
arun789 <adwiti1975@gmail.com>
2018-01-19 22:03:05 -0700

    interview

--- src/main/java/General/OverridingAStaticMethod.java
+++ src/main/java/General/OverridingAStaticMethod.java
@@ -1,4 +1,29 @@
 package General;
 
+/**
+ * Overriding a static method is not possible, it becomes method hiding
+ */
 public class OverridingAStaticMethod {
+    public static void main(String[] args) {
+        A1.m1("arun");
+        B1.m1("adwiti");
+
+        A1 a = new B1();
+        a.m1("Arun"); //this will call A1
+
+        B1 b = new B1();
+        b.m1("Adwiti"); //this will call B1
+    }
+}
+
+class A1 {
+    static void m1(String name) {
+        System.out.println("A1 " + name);
+    }
+}
+
+class B1 extends A1 {
+    static void m1(String name) {
+        System.out.println("B1 " + name);
+    }
 }
